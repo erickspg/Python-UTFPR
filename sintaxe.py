eb3302aa7ba7bbde6b9a409f4a9faf6affd87154
erickspg <erick.spg@gmail.com>
2025-03-19 20:42:06 -0300

    iniciando primeiro commit com sintaxe python

---
+++ sintaxe.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+hora = datetime.now().hour
+
+if hora >= 18:
+    print("Iniciando jornada de aprendizado em python")
+else:
+    print("Ainda irei programar com PHP e JS até as 18h")
