7c662db3f7205ddd7508cd733d291d09f411f340
erickspg <erick.spg@gmail.com>
2025-04-19 19:15:24 -0300

    funcoes

---
+++ python_exemplos/03_estrutura_dados/funcoes.py
@@ -0,0 +1,9 @@
+#Funcoes
+
+def calcularValores(a, b):
+    if (a > b):
+        print(f'Valor {a} é maior que valor {b}')
+    else:
+        print(f'Valor {b} é menor que o valor {b}' )
+
+calcularValores(1,2)
